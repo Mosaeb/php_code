91514449f5583175a5dddf61b17ce00e161715a6
Mosaeb-Bin-Mozib <mosaeb598.@gmail.com>
2023-10-05 14:24:27 +0600

    Navbar and carousel create using bootstrap

---
+++ php-pr-13/app/classes/Home.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\classes;
+
+
+class Home
+{
+    public function index(){
+        return view('home');
+
+    }
+
+}
---
+++ php-pr-13/views/home.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HOME</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+</head>
+<body>
+                    <!--Navbar starts-->
+
+    <nav class="navbar navbar-expand bg-dark navbar-dark">
+        <div class="container">
+            <a href="" class="navbar-brand">LOGO</a>
+            <ul class="navbar-nav mx-auto">
+                <li><a href="" class="nav-link">Home</a></li>
+                <li><a href="" class="nav-link">About</a></li>
+                <li><a href="" class="nav-link">Contact</a></li>
+                <li><a href="" class="nav-link">Porfolio</a></li>
+                <li class="dropdown">
+                    <a href="" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Services</a>
+                    <ul class="dropdown-menu dropdown-menu-dark">
+                        <li><a href="" class="dropdown-item">Service 1</a></li>
+                        <li><a href="" class="dropdown-item">Service 2</a></li>
+                        <li><a href="" class="dropdown-item">Service 3</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li><a href="" class="nav-link">Login</a></li>
+                <li><a href="" class="nav-link">Sign Up</a></li>
+            </ul>
+        </div>
+    </nav>
+                    <!--Navbar end........................-->
+
+                    <!--Carousel starts-->
+
+    <div class="container">
+        <div id="slider" class="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="assets/img/s1.jpg" class="w-100" height="450" alt="">
+                    <div class="carousel-caption">
+                        <h1>Carousel Fair</h1>
+                        <p class="bg-dark">Carousel Fair Ride is a classic amusement ride including a large rotating platform with horse-shaped seats, also called fair merry go round carousel.</p>
+                        <a href="" class="btn btn-success">Read More</a>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/img/s2.jpg" class="w-100"  height="450" alt="">
+                    <div class="carousel-caption">
+                        <h1>Lake Superior</h1>
+                        <p>Lake Superior, one of the top three most scenic Great Lake places, includes rocky cliffs, extensive hiking paths, and distant sea kayaking routes in contrast to Lake Michigan's gentle sand dunes. Every tourist's bucket list to the Lake Superior area should include a visit to the Pictured Rocks.</p>
+                        <a href="" class="btn btn-dark">Read More</a>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/img/s3.jpg" class="w-100"  height="450" alt="">
+                    <div class="carousel-caption">
+                        <h1>Joystick</h1>
+                        <p >A joystick is a hand-held input device used to control the movement or actions of a digital object on a computer screen. It typically consists of a handle or a lever that can be tilted or pushed in different directions to provide input to a computer or a gaming console.</p>
+                        <a href="" class="btn btn-danger">Read More</a>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/img/s4.jpg" class="w-100"  height="450" alt="">
+                    <div class="carousel-caption">
+                        <h1>Seas</h1>
+                        <p>Seas are smaller than oceans and are usually located where the land and ocean meet. Typically, seas are partially enclosed by land. The Sargasso Sea is an exception. It is defined only by ocean currents.</p>
+                        <a href="" class="btn btn-info">Read More</a>
+                    </div>
+                </div>
+            </div>
+            <a href="#slider" class="carousel-control-prev" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+            </a>
+            <a href="#slider" class="carousel-control-next" data-bs-slide="next">
+                <span class="carousel-control-next-icon"></span>
+            </a>
+        </div>
+    </div>
+                    <!--Carousel end....................-->
+
+
+
+
+                    <script src="assets/js/bootstrap.bundle.js"></script>
+</body>
+</html>
